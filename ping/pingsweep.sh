e02bcdd33ecd7ff4a002d7a7e0ed477c941817d0
ShaieneKun <rafaelshaiene115@gmail.com>
2021-10-08 03:12:38 -0300

    Utility that pings local network

---
+++ ping/pingsweep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This is a basic utility that sends a ping to all devices on a 255.255.255.0 masked local network
+# Usage: pingsweep.sh 192.168.0
+
+ip_start=$1 # Get user input
+
+for ip_end in $(seq 1 255)
+do
+	ip="$ip_start.$ip_end"
+	# Sends one ping to ip
+	ping -c 1 $ip | 
+	# Fetchs succesful ping
+	grep "64 bytes"  | 
+	# Cuts only the IP part
+	cut -d " " -f 4  | 
+	# Removes trailing ":" and run the loop on different threads
+	tr -d ":" &
+done
